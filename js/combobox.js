8b5eea431f810bdea75550772267c024e5a12e16
Toshio HIRAI <toshio.hirai@gmail.com>
2018-09-03 18:18:04 +0900

    update script.

--- js/combobox.js
+++ js/combobox.js
@@ -10,113 +10,96 @@
 ;(function($){
     $.fn.combobox = function(options) {
         const self = this;
-
-        this.settings = $.extend( {
-            'text-option-class' : 'text',
-            'input-callback' : null,
+        const settings = $.extend( {
+            'text_option_class' : 'cmb-text',
+            'text_input_callback' : null,
+            'text_initial_string' : '',
+            'text_placeholder' : '',
+            'text_on_change' : null,
+            'text_on_focus' : null,
+            'text_holder' : "data-text",
             'encoder' : encodeURIComponent,
             'decoder' : decodeURIComponent,
-            'encode-value' :true,
-            'placeholder' : '',
-            'initial-text' : ''
+            'encode' :false
         }, options);
 
         this.methods = {
             val : function() {
-                if (self.val() !== "") return self.val();
-                const text = self.attr("data-text");
+                if (!self.find('option:selected').hasClass(settings['text_option_class'])) return self.val();
+                const text = self.attr(settings['text_holder']);
                 if (!text) return '';
-                return self.decode(text);
+                return decode(text);
             }
         };
 
-        this.create = function(element, focus) {
-            const top = element.position().top+1,  left = element.position().left+1, width = element.innerWidth()-18, height = element.innerHeight();
-            const parent = $('<div id="' + self.id(element) + '"></div>').css('position', 'absolute')
-                .css('top', top).css('left', left).css('width', width).css('height', height)
-                .css('line-height', '1.15').css('-webkit-text-size-adjust', '100%').css('margin', 0);
-            const textBox = $('<input type="text" class="cmbInnerText" value="">').css('position', 'relative')
-                .css('top', window.navigator.userAgent.toLowerCase().indexOf('chrome') !== -1 ? '-2px' : '0')
-                .css('left', 0).css('width', '100%').css('height', 'calc(100% - 2px)').css('border', '0px none').css('outline', '0')
-                .css('-moz-box-sizing', 'border-box').css('-webkit-box-sizing', 'border-box').css('box-sizing', 'border-box')
-                .attr('placeholder', this.settings['placeholder']);
-            this.inputHandler(textBox);
-            element.after(parent.append(textBox));
-            if (focus) textBox.focus();
-            return textBox;
+        const encode = function(val) {
+            if (!settings['encode']) return val;
+            if (!settings.encoder) return val;
+            return settings.encoder(val);
         };
 
-        this.encode = function(val) {
-            if (!this.settings['encode-value']) return val;
-            if (!this.settings.encoder) return val;
-            return this.settings.encoder(val);
+        const decode = function(val) {
+            if (!settings['encode']) return val;
+            if (!settings.decoder) return val;
+            return settings.decoder(val);
         };
 
-        this.decode = function(val) {
-            if (!this.settings['encode-value']) return val;
-            if (!this.settings.decoder) return val;
-            return this.settings.decoder(val);
+        const textVal = function(element) {
+            if (element.attr(settings['text_holder']) === undefined) return "";
+            return decode(element.attr(settings['text_holder']))
         };
 
-        this.inputHandler = function(input) {
-            const element = input;
-            element.on(
-                {"input" : function() {
-                    const select = element.parent().prevAll('select:first');
-                    const opt = select.children(':selected');
-                    select.attr('data-text', self.encode(element.val()));
-                    opt.text(element.val() || self.settings['initial-text']);
-                    if (self.settings['input-callback']) self.settings['input-callback']();
-                }})
-                .on('focus', function(){
-                    //this.select();
-                })
-                .on('change', function(){
-                })
-            ;
-        };
-
-        this.textVal = function(element) {
-            if (element.attr('data-text') === undefined) return "";
-            return this.decode(element.attr('data-text'))
-        };
-
-        this.id = function(element) {
+        const id = function(element) {
             return 'cmb-' + element.attr('id');
-        };
-
-        this.on('change', function() {
-            const opt = $(this).children(':selected');
-            const cmbId = self.id($(this)), combo = $('#' + cmbId);
-            if (combo.length) combo.hide();
-            if (!opt.hasClass(self.settings['text-option-class'])) return;
-            combo.show();
-            // opt.text(self.textVal($(this)) );
-            // const textBox = self.create($(this), true);
-            // textBox.val(self.textVal($(this)));
-        });
-
-        this.init = function(element) {
-            const text = self.create(element).val(self.textVal(element));
-            text.closest('div').hide();
-            element.find('input').each(function(){
-                self.inputHandler($(this))
-            });
-            const opt = element.children(':selected');
-            if (opt.hasClass(this.settings['text-option-class'])) {
-                element.children(':selected').text(self.textVal(element));
-                text.closest('div').show();
-            }
         };
 
         if (this.methods[options]) {
             return this.methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof options === 'object' || !options) {
             return this.each(function() {
-                self.init($(this));
+                $(this).on('change', function(e) {
+                    const cmb = $('#' + id($(this))).hide();
+                    if (!$(this).find('option:selected').hasClass(settings['text_option_class'])) return;
+                    cmb.show().find('input').focus();
+                    e.preventDefault();
+                });
+
+                const top = $(this).position().top+1, left = $(this).position().left+1, width = $(this).innerWidth()-18, height = $(this).innerHeight();
+                const cmb = $('<div id="' + id($(this)) + '"></div>').css('position', 'absolute')
+                    .css('top', top).css('left', left).css('width', width).css('height', height)
+                    .css('line-height', '1.15').css('-webkit-text-size-adjust', '100%').css('margin', 0).hide()
+                ;
+                const text = $('<input type="text" class="cmbInnerText" value="">').css('position', 'relative')
+                    .css('top', window.navigator.userAgent.toLowerCase().indexOf('chrome') !== -1 ? '-2px' : '0')
+                    .css('left', 0).css('width', '100%')
+                    .css('height', 'calc(100% - ' + (window.navigator.userAgent.toLowerCase().indexOf('chrome') !== -1 ? '1px' : '2px') + ')')
+                    .css('border', '0px none').css('outline', '0')
+                    .css('-moz-box-sizing', 'border-box').css('-webkit-box-sizing', 'border-box').css('box-sizing', 'border-box')
+                    .attr('placeholder', settings['text_placeholder']).val(textVal($(this)))
+                    .on({"input" : function() {
+                            text.parent().prevAll('select:first')
+                                .attr(settings['text_holder'], encode(text.val()))
+                                .find('option:selected').text(text.val() || settings['text_initial_string'])
+                            ;
+                            if (settings['text_input_callback']) settings['text_input_callback']();
+                        }})
+                        .on('focus', function(){
+                            if (settings['text_on_focus']) settings['text_on_focus']();
+                        })
+                        .on('change', function(){
+                            if (settings['text_on_change']) settings['text_on_change']();
+                        })
+                ;
+
+                $(this).after(cmb.append(text));
+                const opt = $(this).find('option:selected');
+                if (opt.hasClass(settings['text_option_class'])) {
+                    opt.text(textVal($(this)));
+                    cmb.show();
+                }
             });
         } else {
             $.error('Method ' + options + ' does not exist on jQuery.combobox');
         }
     };
-})(jQuery)
+})(jQuery);
